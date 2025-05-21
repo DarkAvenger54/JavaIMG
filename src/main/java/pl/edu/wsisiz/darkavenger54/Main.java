f6beb674bca0341dcd40b76a5fae99284b41330a
DarkAvenger54 <evgen.manuylov@gmail.com>
2025-05-21 08:13:05 +0200

    0.03

--- src/main/java/pl/edu/wsisiz/darkavenger54/Main.java
+++ src/main/java/pl/edu/wsisiz/darkavenger54/Main.java
@@ -3,6 +3,8 @@ package pl.edu.wsisiz.darkavenger54;
 import org.opencv.core.Core;
 import pl.edu.wsisiz.darkavenger54.forms.MainForm;
 
+import java.nio.file.Paths;
+
 /**
  * @author Yevhenii Manuilov
  */
@@ -11,7 +13,19 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        String arch = System.getProperty("os.arch");
+        String libPath;
+        if (arch.contains("64")) {
+            libPath = "lib/native/x64/opencv_java4100.dll";
+        } else {
+            libPath = "lib/native/x86/opencv_java4100.dll";
+        }
+        try {
+            System.load(Paths.get(libPath).toAbsolutePath().toString());
+            System.out.println("Loaded OpenCV native library: " + libPath);
+        } catch (UnsatisfiedLinkError e) {
+            System.err.println("Failed to load native OpenCV library for arch: " + arch);
+        }
         System.out.println("OpenCV Version: " + Core.VERSION);
         MainForm mainForm = new MainForm();
     }
